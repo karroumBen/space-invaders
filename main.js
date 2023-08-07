9bbddebf6444e51b8d9529086db5659ebe2bff02
Abdelkarim Ben Karroum <garitomo@yahoo.fr>
2023-08-07 00:05:58 -0500

    added explosion effect

--- main.js
+++ main.js
@@ -6,16 +6,17 @@ const ground = document.querySelector('#ground');
 const scoreLabel = document.querySelector('#score-label');
 const step = 25;
 let score = 0;
+const width = '60px';
+let isGameOver = false;
 
 gameLauncher.addEventListener('click', (evt) => {
   startGame(evt);
   const initPlayerInterval = setInterval(() => {
-    initPlayer({ src: '/assets/drone.png', type: 'evil'});
+    initPlayer({ src: 'assets/drone.png', type: 'evil'});
     const evilObjects = document.querySelectorAll('.evil-player');
       if(evilObjects.length) { 
         evilObjects.forEach(item => {
           let { y } = item;
-          console.log('top', y);
           y += 40;
           item.style.top = y + 'px';
         })
@@ -23,6 +24,7 @@ gameLauncher.addEventListener('click', (evt) => {
     
     if(isEnemyBreach()) {
       clearInterval(initPlayerInterval);
+      isGameOver = true;
       console.log('you lost');
     }
   }, 2000);
@@ -31,15 +33,15 @@ gameLauncher.addEventListener('click', (evt) => {
 const startGame = (evt) => {
   home.classList.toggle('d-none');
   battleGround.classList.toggle('d-none');
-  initPlayer({ src: '/assets/drone.png', type: 'evil'});
-  initPlayer({ src: '/assets/spaceship.png', type: 'good'});
+  initPlayer({ src: 'assets/drone.png', type: 'evil'});
+  initPlayer({ src: 'assets/spaceship.png', type: 'good'});
 }
 
 const initPlayer = (params) => {
   const { type, src } = params;
   const player = document.createElement('img');
   player.src = src;
-  player.style.width = '80px';
+  player.style.width = width;
   player.style.position = 'absolute';
   
   player.style.zIndex = 3;
@@ -62,7 +64,7 @@ const launchRockets = () => {
   const {left, width} = spaceShip.getBoundingClientRect();
 
   const rocket = document.createElement('img');
-  rocket.src = '/assets/rocket.png';
+  rocket.src = 'assets/rocket.png';
   rocket.style.width = '15px';
   rocket.style.height = '30px';
   rocket.style.position = 'absolute';
@@ -75,17 +77,18 @@ const launchRockets = () => {
 }
 
 document.addEventListener('keyup', (e) => {
+  if(isGameOver) return;
+
   if (e.key == " " ||
       e.code == "Space" ||      
       e.keyCode == 32      
   ) {
     const rocket = launchRockets();
     let { x: rocketX, bottom, y: rocketY } = rocket.getBoundingClientRect();
-    console.log(rocketX, rocketY);
     let currentBottom = 90;
+
     const id = setInterval(() => {
       let { x: rocketX, y: rocketY } = rocket.getBoundingClientRect();
-      console.log(rocketX, rocketY);
       currentBottom += step;
       rocket.style.bottom = currentBottom + 'px';
       if(currentBottom > bottom - 50) {
@@ -97,17 +100,19 @@ document.addEventListener('keyup', (e) => {
       if(evilObjects.length) {
         evilObjects.forEach(item => {
           const { x, y } = item;
-          console.log(x, y, rocketY, rocketX);
+
           if(inRange(rocketY, y, y + 100) && inRange(rocketX, x, x + 80) ) {
-            item.setAttribute('src', 'assets/explode.png')
-            ground.removeChild(item);
+            item.setAttribute('src', 'assets/explode.png');
+            setTimeout(() => {
+              ground.removeChild(item);
+            }, 150);
             score += 10;
             if(score > 500) console.log('Congratulations');
             scoreLabel.textContent = score +'';
           }
         })
       }
-    }, 150);
+    }, 300);
   }
 
   if (e.keyCode == 37) {// move spaceship left
@@ -126,8 +131,6 @@ document.addEventListener('keyup', (e) => {
     const { right } = spaceShip.getBoundingClientRect();
     const { width } = ground.getBoundingClientRect();
     let currentRight = right;
-    console.log(right);
-    console.log(width);
 
     if(right < width - 2 * step) {
       currentRight += step;
@@ -137,7 +140,6 @@ document.addEventListener('keyup', (e) => {
 })
 
 const inRange = (target, min, max) => {
-  console.log(target, min, max);
   if(target >= min && target < max) return true;
   return false;
 }
@@ -151,6 +153,6 @@ const isEnemyBreach = () => {
   const { y } = spaceShip.getBoundingClientRect();
   const evilObjects = document.querySelectorAll('.evil-player');
   if(evilObjects.length) {
-    return Array.from(evilObjects).some(item => item.y >= y - 80);
+    return Array.from(evilObjects).some(item => item.y >= y - 100);
   }
 }
